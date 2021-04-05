1d8cb4280df690961bb0d42805ce83cc5188d978
Sankeerth Gunnala <sankeerth.gunnala@gmail.com>
2021-04-05 11:15:46 +0530

    co working spaces
    second commit

---
+++ cws-backend/config/app.config.js
@@ -0,0 +1,11 @@
+exports.config={
+    PORT:12986,
+   MONGO_URL: "mongodb+srv://sankeerthg:keerthgoud@cluster0.3bzdf.mongodb.net/10amecommerse?retryWrites=true&w=majority",
+//    MONGO_URL: "mongodb://locaclhost/cws",
+    JWT_SECRET:'bdkvkdbkdddjdbv'
+}
+
+
+
+
+
